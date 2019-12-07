ca9e089f2efd2e5358eec6605e310434a9815c48
osman conteh <osmannabieconteh@gmail.com>
2019-12-07 08:17:43 -0500

    newchanges

--- assets/javasrcipt/game.js
+++ assets/javasrcipt/game.js
@@ -4,57 +4,57 @@ var wordGuessGame = {
     
     wordsToPick: {
       genesis: {
-        picture: "genesis.jpg",
+        picture: "MustFitness.jpg",
         song: "Illegal Alien",
         preview: "https://p.scdn.co/mp3-preview/b29a2b925b9654e0efaaff37504fd234307e0ad8"
       },
       madonna: {
-        picture: "madonna.jpg",
+        picture: "img6.jpg",
         song: "Material Girl",
         preview: "https://p.scdn.co/mp3-preview/5ff7f7b7d2af1a747da275bed3c49054c01b5b1c"
       },
       toto: {
-        picture: "toto.jpg",
-        song: "Rosanna",
+        picture: "",
+        song: "img7.jpg",
         preview: "https://p.scdn.co/mp3-preview/7cef811eaeb7c7b98245750e73d9d68e9008f317"
       },
       queen: {
-        picture: "queen.jpg",
+        picture: "images3.jpg",
         song: "Princes of the Universe",
         preview: "https://p.scdn.co/mp3-preview/b84f24300476f3d3f20056d2388cc51db2e3891f"
       },
       u2: {
-        picture: "u2.jpg",
+        picture: "images4.jpg",
         song: "With or Without You",
         preview: "https://p.scdn.co/mp3-preview/28365dff1890075c1371628371cd0e5bbff9a3a3"
       },
       metallica: {
-        picture: "metallica.jpg",
+        picture: "images6.jpg",
         song: "Master of Puppets",
         preview: "https://p.scdn.co/mp3-preview/60e6f8dab43f176dd9fb5e795d4e6459bad52e9e"
       },
       journey: {
-        picture: "journey.jpg",
+        picture: "images8.jpg",
         song: "Don't Stop Believin'",
         preview: "https://p.scdn.co/mp3-preview/21b9abd3cd2eea634e17a917196fdd5ba2e82670"
       },
       inxs: {
-        picture: "inxs.jpg",
+        picture: "imgs.7.jpg",
         song: "Need You Tonight",
         preview: "https://p.scdn.co/mp3-preview/61b17a335d5afc1c4086b1b08e2400f0da147977"
       },
       poison: {
-        picture: "poison.jpg",
+        picture: "im6.jpg",
         song: "Fallen Angel",
         preview: "https://p.scdn.co/mp3-preview/0365ad1f152f1834b42b857c4625191cebf9f987"
       },
       rush: {
-        picture: "rush.jpg",
+        picture: "im7.jpg",
         song: "Limelight",
         preview: "https://p.scdn.co/mp3-preview/154987dfb07f2fc5ed7aa4d76b80c5dc08ff4d6b"
       },
       blondie: {
-        picture: "blondie.jpg",
+        picture: "MustFitnes.jpg",
         song: "Call Me",
         preview: "https://p.scdn.co/mp3-preview/ed5a443bc86176135ebca8a114f66f4d814d4c90"
       }
